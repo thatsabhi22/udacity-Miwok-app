72d56d5563351aa19637cecf26a378f6a870318d
The Aviator <abhi@xyz>
2017-12-17 08:15:34 +0530

    All Activities finally polished visually

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,21 +19,26 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
+    /** Resource ID for background color of the list of words */
+    private int mColorResourceId;
+
     /**
      * This is our own custom constructor (it doesn't mirror a superclass constructor).
      * The context is used to inflate the layout file, and the list is the data we want
      * to populate into the lists.
-     *
-     * @param context        The current context. Used to inflate the layout file.
+     *  @param context        The current context. Used to inflate the layout file.
      * @param wordList       A List of Word objects to display in a list
+     * @param background_color  The background color of the list provided from the activity
      */
-    public WordAdapter(Activity context, ArrayList<Word> wordList) {
+    public WordAdapter(Activity context, ArrayList<Word> wordList, int background_color) {
 
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, wordList);
+
+        mColorResourceId = background_color;
     }
 
     /**
@@ -58,6 +64,15 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         // Get the {@link Word} object located at this position in the list
         Word currentWord = getItem(position);
+
+        // Find the LinearLayout in the list_item.xml layout with the ID textContainer
+        View textContainer  = listItemView.findViewById(R.id.textContainer);
+
+        //Get the color from the variable from class
+        int color = ContextCompat.getColor(getContext(),mColorResourceId);
+
+        //Set the Background color of the LinearLayout based on the input
+        textContainer.setBackgroundColor(color);
 
         // Find the TextView in the list_item.xml layout with the ID english_word
         TextView defaultTranslation = (TextView)listItemView.findViewById(R.id.english_word);
