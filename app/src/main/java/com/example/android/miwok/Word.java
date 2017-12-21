160b7820d5760af1c9ea4061fa734bb25b74732c
The Aviator <abhi@xyz>
2017-12-22 03:13:12 +0530

    All 4 Activities incorporated with the audiomanager implementation to handle interrupts while audio is playing
    The list_item_layout is updated with adding a play icon also the its linearlayout changed to relativelayout
    
    All the mp3 files for the words in all 4 activities added to the raw folder
    
    Every drawable version of the play icon added to the drawable folder

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,89 +4,129 @@ package com.example.android.miwok;
  * Created by aviator on 15/12/17.
  */
 
-    /**
-    * {@link Word} represents a vocabulary word that the user wants to learn.
-    * It contains a default translation and a Miwok translation for that word.
-    */
-    public class Word {
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ */
+public class Word {
 
-    /** Default translation for the word */
+    /**
+     * Default translation for the word
+     */
     private String mDefaultTranslation;
 
-    /** Miwok translation for the word */
+    /**
+     * Miwok translation for the word
+     */
     private String mMiwokTranslation;
 
-    /** List Item supporting image for the word */
+    /**
+     * List Item supporting image for the word
+     */
     private int mImageResourceId;
 
+    /**
+     * List Item supporting audio for the word
+     */
+    private int mAudioResourceId;
 
     /**
-     +     * Create a new Word object.
-     +     *
-     +     * @param defaultTranslation is the word in a language that the user is already familiar with
-     +     *                           (such as English)
-     +     * @param miwokTranslation is the word in the Miwok language
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with (such as English)
+     * @param miwokTranslation   is the word in the Miwok language
      */
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
     }
 
-     /**
-     +     * Create a new Word object.
-     +     *
-     +     * @param defaultTranslation is the word in a language that the user is already familiar with
-     +     *                           (such as English)
-     +     * @param miwokTranslation is the word in the Miwok language
-     +     * @param imageResourceId image Resource id for the image on the wordlist
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with(such as English)
+     * @param miwokTranslation   is the word in the Miwok language
+     * @param imageResourceId    image Resource id for the image on the wordlist
+     * @param audioResourceId    image Resource id for the image on the wordlist
      */
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
     }
 
 
     /**
-    * Get the Miwok translation of the word.
-    */
+     * Get the Miwok translation of the word.
+     */
     public String getmMiwokTranslation() {
         return mMiwokTranslation;
     }
 
     /**
-    * Set the default translation of the word.
-    */
+     * Set the default translation of the word.
+     */
     public void setmMiwokTranslation(String mMiwokTranslation) {
         this.mMiwokTranslation = mMiwokTranslation;
     }
 
 
     /**
-    * Get the default translation of the word.
-    */
+     * Get the default translation of the word.
+     */
     public String getmDefaultTranslation() {
         return mDefaultTranslation;
     }
 
     /**
-    * Set the default translation of the word.
-    */
+     * Set the default translation of the word.
+     */
     public void setmDefaultTranslation(String mDefaultTranslation) {
         this.mDefaultTranslation = mDefaultTranslation;
     }
 
     /**
-    * Get List Item supporting image for the word
-    */
+     * Get List Item supporting image for the word
+     */
     public int getImageResourceId() {
         return mImageResourceId;
     }
 
     /**
-    * Set List Item supporting image for the word
-    */
+     * Set List Item supporting image for the word
+     */
     public void setImageResourceId(int imageResourceId) {
         this.mImageResourceId = imageResourceId;
+    }
+
+    /**
+     * Get List Item supporting audio for the word
+     */
+    public int getmAudioResourceId() {
+        return mAudioResourceId;
+    }
+
+    /**
+     * Set List Item supporting audio for the word
+     */
+    public void setmAudioResourceId(int audioResourceId) {
+        this.mAudioResourceId = audioResourceId;
+    }
+
+    /**
+     * Returns the string representation of the {@link Word} object.
+     *
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", mImageResourceId=" + mImageResourceId +
+                ", mAudioResourceId=" + mAudioResourceId +
+                '}';
     }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -19,16 +19,19 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    /** Resource ID for background color of the list of words */
+    /**
+     * Resource ID for background color of the list of words
+     */
     private int mColorResourceId;
 
     /**
      * This is our own custom constructor (it doesn't mirror a superclass constructor).
      * The context is used to inflate the layout file, and the list is the data we want
      * to populate into the lists.
-     *  @param context        The current context. Used to inflate the layout file.
-     * @param wordList       A List of Word objects to display in a list
-     * @param background_color  The background color of the list provided from the activity
+     *
+     * @param context          The current context. Used to inflate the layout file.
+     * @param wordList         A List of Word objects to display in a list
+     * @param background_color The background color of the list provided from the activity
      */
     public WordAdapter(Activity context, ArrayList<Word> wordList, int background_color) {
 
@@ -44,10 +47,10 @@ public class WordAdapter extends ArrayAdapter<Word> {
     /**
      * Provides a view for an AdapterView (ListView, GridView, etc.)
      *
-     * @param position The position in the list of data that should be displayed in the
-     *                 list item view.
+     * @param position    The position in the list of data that should be displayed in the
+     *                    list item view.
      * @param convertView The recycled view to populate.
-     * @param parent The parent ViewGroup that is used for inflation.
+     * @param parent      The parent ViewGroup that is used for inflation.
      * @return The View for the position in the AdapterView.
      */
     @NonNull
@@ -66,33 +69,33 @@ public class WordAdapter extends ArrayAdapter<Word> {
         Word currentWord = getItem(position);
 
         // Find the LinearLayout in the list_item.xml layout with the ID textContainer
-        View textContainer  = listItemView.findViewById(R.id.textContainer);
+        View textContainer = listItemView.findViewById(R.id.text_container);
 
         //Get the color from the variable from class
-        int color = ContextCompat.getColor(getContext(),mColorResourceId);
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
 
         //Set the Background color of the LinearLayout based on the input
         textContainer.setBackgroundColor(color);
 
         // Find the TextView in the list_item.xml layout with the ID english_word
-        TextView defaultTranslation = (TextView)listItemView.findViewById(R.id.english_word);
+        TextView defaultTranslation = (TextView) listItemView.findViewById(R.id.english_word);
 
         // Get the default translation from current Word object and
         // set this text on the name TextView
         defaultTranslation.setText(currentWord.getmDefaultTranslation());
 
         // Find the TextView in the list_item.xml layout with the ID miwok_word
-        TextView miwokTranslation = (TextView)listItemView.findViewById(R.id.miwok_word);
+        TextView miwokTranslation = (TextView) listItemView.findViewById(R.id.miwok_word);
 
         // Get the miwok translation from the current Word object and
         // set this text on the name TextView
         miwokTranslation.setText(currentWord.getmMiwokTranslation());
 
         // Find the ImageView in the list_item.xml layout with the ID list_image
-        ImageView wordImage = (ImageView)listItemView.findViewById(R.id.list_image);
+        ImageView wordImage = (ImageView) listItemView.findViewById(R.id.list_image);
 
         //If image resource not found visibility of the ImageView set to GONE
-        if(currentWord.getImageResourceId() == 0)
+        if (currentWord.getImageResourceId() == 0)
             wordImage.setVisibility(View.GONE);
         else
             wordImage.setImageResource(currentWord.getImageResourceId());
